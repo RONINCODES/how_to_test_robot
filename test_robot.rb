46b0caaff2f778d8ec93a8d6091d1e39e3dc6111
Ronen Annason <ronenannason83@gmail.com>
2017-06-22 17:26:38 -0400

    exercise completed

--- test_robot.rb
+++ test_robot.rb
@@ -4,76 +4,121 @@ require './robot.rb'
 
 class TestRobot < MiniTest::Test
 
-  def test_that_foreign_robot_neeing_repairs_sent_to_station_1
-    skip
+
+
+  def test_that_foreign_robot_needing_repairs_sent_to_station_1
+
     # arrange
+    @robot = Robot.new #setup instance of new robot
+    @robot.needs_repairs = true
+    @robot.foreign_model = true
 
     # act
+    station = @robot.station
+
 
     # assert
+    assert_equal(station, 1)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
+
     # arrange
+    @robot = Robot.new #setup instance of new robot
+    @robot.needs_repairs = true
+    @robot.vintage_model = true
 
     # act
+    station = @robot.station
 
     # assert
+    assert_equal(station, 2)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
+
     # arrange
+    @robot = Robot.new #setup instance of new robot
+    @robot.needs_repairs = true
+    @robot.vintage_model = false
+    @robot.foreign_model = false
+
 
     # act
+    station = @robot.station
 
     # assert
+    assert_equal(station, 3)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
+
     # arrange
+    @robot = Robot.new #setup instance of new robot
+    @robot.needs_repairs = false
+    @robot.vintage_model = false
+    @robot.foreign_model = false
 
     # act
+    station = @robot.station
 
     # assert
+    assert_equal(station, 4)
+
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
+
     # arrange
+    @robot = Robot.new
+    @robot.todos = []
 
     # act
+    task_priority = @robot.prioritize_tasks
 
     # assert
+    assert_equal(task_priority, -1)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
+
     # arrange
 
+    @robot = Robot.new
+    @robot.todos = [1,2,5]
+
     # act
+    task_priority = @robot.prioritize_tasks
 
     # assert
+    assert_equal(task_priority, @robot.todos.max)
+
   end
 
   def test_workday_on_day_off_returns_true
-    skip
+
     # arrange
+    @robot = Robot.new
+    @robot.day_off = 'Friday'
 
     # act
+    work = @robot.workday?('Monday')
 
     # assert
+    assert_equal(work,true)
   end
 
   def test_workday_not_day_off_returns_false
-    skip
+
     # arrange
+    @robot = Robot.new
+    @robot.day_off = 'Tuesday'
 
     # act
+    work = @robot.workday?('Tuesday')
 
     # assert
+    assert_equal(work,false)
   end
 
 end
